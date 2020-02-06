fef592bab44c7e7fe0621cde8b271ba4176f3425
Tolu Adesegha <tolu@adesegha.ca>
2020-02-06 01:47:43 -0700

    add hammerspoon rc

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
+directory=$(dirname $0)
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && \
-    brew cask install hammerspoon
+    brew cask install hammerspoon && \
+    mkdir ~/.hammerspoon && \
+    ln -s $directory/hammerspoonrc ~/.hammerspoon/init.lua
