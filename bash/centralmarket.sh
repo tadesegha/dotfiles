420fa8f41d382a2a8f23761016f67dae3b68be4d
Tolu Adesegha <tolu@adesegha.ca>
2021-01-13 10:00:01 -0700

    synchronizing repositories

--- bash/centralmarket.sh
+++ bash/centralmarket.sh
@@ -1,5 +1,5 @@
 alias cm="cd ~/dev/central-market"
-alias cmwp="cd ~/dev/central-market/wordpress/centralmarket.com/html"
+alias cmwp="cd ~/dev/central-market/centralmarket.com"
 alias cmrs="cd ~/dev/central-market/react-service"
 alias cm-connect-container="docker-command docker exec -it centralmarket.com bash"
 alias rdcli="~/.config/yarn/global/node_modules/.bin/rdcli"
@@ -19,7 +19,7 @@ function cm-continue-mr() {
 }
 
 # central market aliases
-alias cm-switch-branch='cd ~/dev/central-market/wordpress/centralmarket.com/html && composer install && cd wp-content/themes/centralmarket-theme/ && npm ci && npm run prod && echo consider running "docker-command docker exec centralmarket.com wp plugin activate centralmarket-react-service"'
+alias cm-switch-branch='cd ~/dev/central-market/centralmarket.com && composer install && cd wp-content/themes/centralmarket-theme/ && npm ci && npm run prod && echo consider running "docker-command docker exec centralmarket.com wp plugin activate centralmarket-react-service"'
 
 # central market urls
 alias cm-request-fulfillment-bar="curl http://localhost:3000/batch -H 'content-type: application/json' -d '{\"state\": {},\"components\":{\"FulfillmentBar\":{\"component\":\"FulfillmentBar\",\"data\": {\"session\": {\"user\": {\"display_name\": \"Nenad\",\"memberships\": []},\"channels\": {\"curbside\": {\"store\": {\"id\": 420,\"address\": {\"company\": \"Austin Westgate\",\"address_1\": \"4477 SOUTH LAMAR\",\"address_2\": \"\",\"city\": \"AUSTIN\",\"state\": \"TX\",\"postcode\": \"78745\",\"country\": \"US\"},\"phone\": \"1-737-708-8377\",\"channels\": [\"curbside\",\"holiday-hotline\"],\"is_default\": false},\"schedule\": {\"type\": \"pickup\",\"timeslot\": {\"from\": \"2020-07-24T20:30:00-05:00\",\"until\": \"2020-07-24T21:00:00-05:00\",\"expires\": \"2020-07-24T14:38:59-05:00\"}},\"open_orders\": [{\"id\": 1024466,\"type\": \"delivery\",\"addon_until\": \"2020-07-27T14:00:00-05:00\",\"addons_available\": 4,\"store\": {\"id\": 491,\"address\": {\"company\": \"Houston\",\"address_1\": \"3815 WESTHEIMER\",\"address_2\": \"\",\"city\": \"HOUSTON\",\"state\": \"TX\",\"postcode\": \"77027\",\"country\": \"US\"}},\"timeslot\": {\"from\": \"2020-07-27T18:00:00-05:00\",\"until\": \"2020-07-27T19:00:00-05:00\"},\"delivery_address\": {\"address_1\": \"1504 Ben Taub Loop\"}}],\"cart_count\": 10}},\"max_add_on_items\": 10},\"buttonLabel\": \"edit\",\"topCtaMessage\": \"\",\"topCtaLinkTo\": \"\",\"bottomCtaMessage\": \"*Pricing and availability may change depending on fulfillment selection\"}}}}' | jq"
